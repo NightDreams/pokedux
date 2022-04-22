66f24197a8ffa9e302c37a9363c7b99b89fa115c
NightDreams <yayodcv@gmail.com>
2022-04-21 19:04:48 -0500

    C8 - Setup Redux , reduxToolkit + react-redux , 2022/04

--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App';
-import './index.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from "react-redux";
+
+import { pokemonReducer } from "./reducers/pokemon";
+
+import "./index.css";
+
+const store = configureStore({
+  reducer: pokemonReducer,
+  // devTools: true,
+});
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />,
+  </Provider>,
+  document.getElementById("root")
+);
