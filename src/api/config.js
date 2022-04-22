a3dbcd63bfde71b93bd917d3c33fe5e5ed5b9efb
NightDreams <yayodcv@gmail.com>
2022-04-21 22:56:33 -0500

    c11 - Setup async queries - Promise.All and conect data to PokemonCards

--- src/api/config.js
+++
@@ -1,8 +0,0 @@
-import { create } from "axios";
-
-const pokeapi = "https://pokeapi.co/api/v2/";
-const url = process.env.POKEAPI || pokeapi;
-
-export const axiosInstance = create({
-  baseURL: url,
-});
--- src/components/PokemonList/PokemonCard.js
+++ src/components/PokemonList/PokemonCard.js
@@ -8,10 +8,14 @@ const PokemonCard = ({ pokemon }) => {
     <Grid.Column mobile={16} tablet={8} computer={4}>
       <div className="PokemonCard">
         <Icon name="favorite" color={FAV_COLOR}></Icon>
-        <Image centered src={API_URL}></Image>
+        <Image centered src={pokemon.sprites.front_default}></Image>
         <h2 className="PokemonCard-title">{pokemon.name}</h2>
         <Divider />
-        <Label color={MAIN_COLOR}>Normal</Label>
+        {pokemon.types.map((type) => (
+          <Label color={MAIN_COLOR} key={`${pokemon.id}-${type.type.name} `}>
+            {type.type.name}
+          </Label>
+        ))}
       </div>
     </Grid.Column>
   );
